59531e46e1008977469f73b3a4f959d0b13a0cbf
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-31 23:48:21 +0530

    ping pong

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8000 });
 
 wss.on("connection" ,function(socket){
-    console.log("userConnected");
-    setInterval(()=>{
-        socket.send("Current Price Of Gold : " + (Math.random()*1000))
-    }, 5000)
 
     socket.on("message" , (e)=>{
-        console.log(e.toString());
-        
+        if(e.toString() == "ping"){
+            socket.send("pong"); 
+        };
     })
+
 })
