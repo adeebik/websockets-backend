fa6be2580d6b543da12271fe1502208fa5d510bd
Khan-Adeeb <adeebkhan2.786@gmail.com>
2026-01-05 19:05:00 +0530

    backend

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,110 @@
 import { WebSocket, WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8000 });
 
-const allRooms = new Map<string, Set<WebSocket>>();
-const user = new Map<WebSocket, string>();
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) => {
-  socket.on("error", console.error);
+const rooms = new Map<string, Set<WebSocket>>();
 
-  socket.on("message", (msg) => {
-    const parsedmsg = JSON.parse(msg.toString());
+type userInfo = {
+  room: string;
+  name: string;
+};
+const users = new Map<WebSocket, userInfo>();
 
-    if (parsedmsg.type === "join") {
-      const roomName = parsedmsg.payload.roomId;
-      if (!allRooms.has(roomName)) {
-        allRooms.set(roomName, new Set());
+wss.on("connection", (webSocket) => {
+  webSocket.on("error", (e) => {
+    console.log("Error occurred :", e);
+  });
+
+  webSocket.on("message", (raw) => {
+    const msg = JSON.parse(raw.toString());
+
+    if (msg.type === "join") {
+      const { roomId, name } = msg.payload;
+
+      if (!rooms.has(roomId)) {
+        const data = JSON.stringify({
+          type: "system",
+          text: "Room does not exist, Please create",
+        });
+        return webSocket.send(data);
       }
 
-      allRooms.get(roomName)?.add(socket);
-      user.set(socket, roomName);
+      rooms.get(roomId)?.add(webSocket);
+      users.set(webSocket, { room: roomId, name: name });
+
+      const data = JSON.stringify({
+        type: "system",
+        text: `${name} joined the room`,
+      });
+
+      rooms.get(roomId)?.forEach((socket) => {
+        socket.send(data);
+      });
+
+      return;
     }
 
-    if (parsedmsg.type === "chat") {
-      const messgae = parsedmsg.payload.text;
-      const roomName = user.get(socket);
-      if (roomName) {
-        allRooms.get(roomName)?.forEach((members) => {
-          if (members !== socket) {
-            members.send(messgae);
-          }
+    if (msg.type === "create") {
+      const { roomId, name } = msg.payload;
+
+      if (rooms.has(roomId)) {
+        const data = JSON.stringify({
+          type: "system",
+          text: "Room already exist, Please join!",
         });
+        return webSocket.send(data);
       }
+
+      rooms.set(roomId, new Set());
+      rooms.get(roomId)?.add(webSocket);
+      users.set(webSocket, { room: roomId, name: name });
+
+      const data = JSON.stringify({
+        type: "system",
+        text: `${name} joined the room`,
+      });
+
+      rooms.get(roomId)?.forEach((socket) => {
+        socket.send(data);
+      });
+
+      return;
+    }
+
+    if (msg.type === "chat") {
+      const msgText = msg.payload.text;
+      const userInfo = users.get(webSocket);
+
+      if (!userInfo) return;
+
+      const { room, name } = userInfo;
+
+      const data = JSON.stringify({
+        type: "chat",
+        payload: {
+          name,
+          text: msgText,
+        },
+      });
+
+      rooms.get(room)?.forEach((member) => {
+        if (member !== webSocket) {
+          member.send(data);
+        }
+      });
     }
   });
 
-  socket.on("close", () => {
-    const roomName = user.get(socket);
-    if (roomName) {
-      allRooms.get(roomName)?.delete(socket);
-      if (allRooms.get(roomName)?.size === 0) {
-        allRooms.delete(roomName);
+  webSocket.on("close", () => {
+    const userInfo = users.get(webSocket);
+    if (userInfo) {
+      const { room } = userInfo;
+      rooms.get(room)?.delete(webSocket);
+
+      if (rooms.get(room)?.size === 0) {
+        rooms.delete(room);
       }
     }
-    user.delete(socket);
+    users.delete(webSocket);
   });
 });
